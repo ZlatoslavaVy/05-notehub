3e7ef416a914d5cce62c9c4c1c27a7f6e774f3e8
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-08-18 11:31:24 +0300

    hide App mutation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,26 @@
 import css from "../App/App.module.css";
 import { useMutation } from "@tanstack/react-query";
+import SearchBox from "../SearchBox/SearchBox";
+import Pagination from "../Pagination/Pagination";
 
 function App() {
-  const mutation = useMutation({
-    mutationFn: async (data) => {
-      const res = await axios.post("");
-    },
-    onSuccess: (data) => {
-      //
-    },
-  });
+  //   const mutation = useMutation({
+  //     mutationFn: async (data) => {
+  //       const res = await axios.post("");
+  //     },
+  //     onSuccess: (data) => {
+  //       //
+  //     },
+  //   });
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
         {/* Компонент SearchBox */}
+        <SearchBox />
         {/* Пагінація */}
+        <Pagination />
         {/* Кнопка створення нотатки */}
+        <button className={css.button}>Create note +</button>
       </header>
     </div>
   );
